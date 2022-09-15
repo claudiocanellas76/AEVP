c7354ebd4f8f0a5ce74bb2408965f92458c252f7
claudiocanellas76 <claudiocanellas@yahoo.com.br>
2022-09-15 00:03:43 -0300

    Teste6

---
+++ models/atleta.go
@@ -0,0 +1,39 @@
+package models
+
+import "github.com/AEVP/db"
+
+type Atleta struct {
+	Id   int
+	Nome string
+}
+
+func getAtletas() []Atleta {
+
+	db := db.ConectaBd
+	getAtleta, err := db.Query("select * from atleta")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	a := Atleta{}
+	as := []Atleta{}
+
+	for getAtleta.Next() {
+
+		var atletaId int
+		var nome string
+
+		err = getAtleta.Scan(&atletaId, &nome)
+		if err != nil {
+			panic(err.Error())
+		}
+		a.Id = atletaId
+		a.Nome = nome
+
+		as = append(as, a)
+
+	}
+	defer db.Close()
+	return as
+
+}
